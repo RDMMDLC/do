8c41af91cea84d301ccfdb4f1ad50fd37327bff7
RDMMDLC <58237652+RDMMDLC@users.noreply.github.com>
2021-01-29 19:07:13 +0100

    Update rplant.sh

--- rplant.sh
+++ rplant.sh
@@ -1,1 +1,2 @@
 ./cpuminer-avx512 -a cpupower -o stratum+tcp://stratum-eu.rplant.xyz:7029 --cpu-affinity 0x3 --cpu-priority 3 -u CefFxmycWpVoFuqeTbZssBsCUipkfV1r5Y.debian10
+CQfqe33EiSEKgjC5HZyt9joZBPtoqCDK6N
